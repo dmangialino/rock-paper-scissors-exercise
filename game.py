29d5b38ef3edf69a7cd75874d4ae7f0fa8f65bc7
Dina Mangialino <dam566@stern.nyu.edu>
2021-05-25 20:37:24 -0400

    Added user input validation

--- game.py
+++ game.py
@@ -1,10 +1,21 @@
 # game.py
 
-print("Rock, Paper, Scissors, Shoot!")
+print("Welcome to Rock, Paper, Scissors, Shoot!")
 
 # Prompt user to enter a chocice, i.e., rock, paper, or scissors
 user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 print("USER CHOICE:", user_choice)
 
-# Validate that the user inputted a valid choice, i.e., rock, paper, or scissors
-# If so, continue with the game. If not, end the game
+
+# Validate that the user inputted a valid choice (i.e., rock, paper, or scissors)
+# If the input is valid, will continue with the rest of the program
+# If selection is invalid, will notify user of invalid input and exit the program
+if (user_choice == "rock") or (user_choice == "paper") or (user_choice == "scissors"):
+    print("VALID CHOICE! KEEP GOING")
+else:
+    print("Oops! Invalid input. Please re-run the program to try again.")
+    exit()
+
+
+
+print("This is the end of our game. Please play again!")
