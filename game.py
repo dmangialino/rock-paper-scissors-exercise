2c41d22bd6e455b091dfaccfcb7570e64fbbb5c0
Dina Mangialino <dam566@stern.nyu.edu>
2021-05-25 20:14:37 -0400

    Process user input

--- game.py
+++ game.py
@@ -1,3 +1,10 @@
 # game.py
 
 print("Rock, Paper, Scissors, Shoot!")
+
+# Prompt user to enter a chocice, i.e., rock, paper, or scissors
+user_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
+print("USER CHOICE:", user_choice)
+
+# Validate that the user inputted a valid choice, i.e., rock, paper, or scissors
+# If so, continue with the game. If not, end the game
